5ade0490a5325ff93676dd231be7e1ce855dd5ff
Steve Jamerson <stevejamerson@outlook.com>
2021-09-18 19:15:47 -0300

    feat(auth/pages/login): carousel added

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -5,7 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
+
+  slides = [
+    {
+      "id": 1,
+      "image": "/assets/images/login/Data@2x.png",
+      "title": "Marcenas mattis egestas",
+      "texto": "Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.",
+    },
+    {
+      "id": 2,
+      "image": "/assets/images/login/Data@2x.png",
+      "title": "Marcenas mattis egestas",
+      "texto": "Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.",
+    },
+    {
+      "id": 3,
+      "image": "/assets/images/login/Data@2x.png",
+      "title": "Marcenas mattis egestas",
+      "texto": "Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.",
+    },
+    {
+      "id": 4,
+      "image": "/assets/images/login/Data@2x.png",
+      "title": "Marcenas mattis egestas",
+      "texto": "Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.",
+    },
+  ]
 
   constructor() { }
 
